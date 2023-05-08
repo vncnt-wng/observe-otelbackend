58f5c171969b81fb68e9275eef1db0848cc5b143
Vi <viwang@Vis-MacBook-Pro.local>
2023-05-08 20:56:49 +0100

    Adds execution path string to every trace

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from typing import Dict, List, Set
+from typing import Dict
 from flask import Flask, request, json, Response
-from time import sleep
+from google.protobuf import text_format
 from datetime import datetime, timedelta
 from opentelemetry.proto.trace.v1 import trace_pb2
-from google.protobuf import text_format
 from google.protobuf.json_format import MessageToDict
 import json
 import pymongo
 from flask_cors import cross_origin, CORS
 import re
 from pymongo_get_database import get_database
+
+from trace_processor import get_trace_dicts
+
 db = get_database()
 
 PORT = 8000
@@ -18,13 +20,15 @@ PORT = 8000
 app = Flask(__name__)
 CORS(app)
 
+
 @app.before_request
 def handle_preflight():
     if request.method == "OPTIONS":
         res = Response()
-        res.headers['X-Content-Type-Options'] = '*'
+        res.headers["X-Content-Type-Options"] = "*"
         return res
-    
+
+
 # @app.after_request
 # def add_header(res):
 #     if res.method != "OPTIONS":
@@ -32,148 +36,112 @@ def handle_preflight():
 #         return res
 
 
-
-@app.route('/get_trace_trees', methods=['POST'])
+@app.route("/get_trace_trees", methods=["POST"])
 @cross_origin()
 def get_trace_trees():
     data = json.loads(request.data)
-    timesByPath = get_children_for_root(data['rootPath'])
-    return {'timesByPath': timesByPath}
+    timesByPathByTree = get_children_for_root(data["rootPath"])
+    return {"timesByPathByTree": timesByPathByTree}
+
 
 def get_children_for_root(rootId: str) -> Dict:
-    collection = db['OtelBackend']['Traces']
-    regex_string = re.compile(f'(,)?{rootId},([^.]+)')
-    traces_cursor = collection.find({'path': {'$regex': regex_string}}, projection={'_id': False})
-    timesByPath = {}
+    print(rootId)
+    collection = db["OtelBackend"]["Traces"]
+    regex_string = re.compile(f"(,)?{rootId}([^.]+)?")
+    traces_cursor = collection.find(
+        {"path": {"$regex": regex_string}}, projection={"_id": False}
+    )
+    timesByPathByTree = {}
     for trace in traces_cursor:
-        if trace['path'] in timesByPath:
-            timesByPath[trace['path']].append(trace)
+        executionPathString = trace["executionPathString"]
+        if executionPathString in timesByPathByTree:
+            timesByPath = timesByPathByTree[executionPathString]
+            if trace["path"] in timesByPath:
+                timesByPath[trace["path"]].append(trace)
+            else:
+                timesByPath[trace["path"]] = [trace]
         else:
-            timesByPath[trace['path']] = [trace]
-    return timesByPath
+            timesByPathByTree[executionPathString] = {}
+            timesByPathByTree[executionPathString][trace["path"]] = [trace]
+        print(timesByPathByTree)
+    return timesByPathByTree
 
-# Change to GET - query parameters? 
-@app.route('/get_mean_response_time', methods=['POST'])
+
+# Change to GET - query parameters?
+@app.route("/get_mean_response_time", methods=["POST"])
 @cross_origin()
 def get_mean_response_time():
     data = json.loads(request.data)
     traces_cursor = query_traces(
-        qualName=data['qualName'], filePath=data['filePath'], prevDays=data['prevDays'])
+        qualName=data["qualName"], filePath=data["filePath"], prevDays=data["prevDays"]
+    )
     total_response_time = 0
     no_traces = 0
     for trace in traces_cursor:
-        total_response_time += trace['responseTime']
+        total_response_time += trace["responseTime"]
         no_traces += 1
-        
-    if no_traces > 0:
-        return {'meanResponseTime': total_response_time/no_traces}, 200
-    return {'meanResponseTime': 0}, 200
 
-@app.route('/get_file_overview', methods=['POST'])
+    if no_traces > 0:
+        return {"meanResponseTime": total_response_time / no_traces}, 200
+    return {"meanResponseTime": 0}, 200
+
+
+@app.route("/get_file_overview", methods=["POST"])
 @cross_origin()
 def get_file_overview():
     data = json.loads(request.data)
-    traces_cursor = query_traces(
-        filePath=data['filePath'], prevDays=data['prevDays']
-    )
+    traces_cursor = query_traces(filePath=data["filePath"], prevDays=data["prevDays"])
     totals = {}
     counts = {}
-    
+
     for trace in traces_cursor:
-        qualName = trace['qualName']
+        qualName = trace["qualName"]
         if qualName not in totals:
             totals[qualName] = 0
             counts[qualName] = 0
-        totals[qualName] += trace['responseTime']
+        totals[qualName] += trace["responseTime"]
         counts[qualName] += 1
-    
-    means = {key: totals[key]/counts[key] for key in totals.keys()}
+
+    means = {key: totals[key] / counts[key] for key in totals.keys()}
     return means
 
-@app.route('/get_all_for_function', methods=['POST'])
+
+@app.route("/get_all_for_function", methods=["POST"])
 @cross_origin()
 def get_all_for_functon():
     data = json.loads(request.data)
     traces_cursor = query_traces(
-        qualName=data['qualName'], filePath=data['filePath'], prevDays=data['prevDays'])
+        qualName=data["qualName"], filePath=data["filePath"], prevDays=data["prevDays"]
+    )
     return list(traces_cursor)
-    
-def query_traces(qualName = None, filePath = None, prevDays=7) -> pymongo.cursor:
+
+
+def query_traces(qualName=None, filePath=None, prevDays=7) -> pymongo.cursor:
     # TODO include file path in call
     query_date_start = datetime.now() - timedelta(days=prevDays)
 
-    query = {'timestamp': {'$gte': query_date_start}}
+    query = {"timestamp": {"$gte": query_date_start}}
     if qualName is not None:
-        query['qualName'] = qualName
+        query["qualName"] = qualName
     if filePath is not None:
         matchString = re.escape(filePath)
-        query['file'] = {'$regex': f'.*{matchString}'}
-    collection = db['OtelBackend']['Traces']
+        query["file"] = {"$regex": f".*{matchString}"}
+    collection = db["OtelBackend"]["Traces"]
 
-    return collection.find(query, projection={'_id': False})
+    return collection.find(query, projection={"_id": False})
 
 
-@app.route('/v1/traces', methods=['POST'])
+@app.route("/v1/traces", methods=["POST"])
 def traces():
     trace = trace_pb2.TracesData()
     trace.ParseFromString(request.data)
 
     trace_dicts = get_trace_dicts(MessageToDict(trace))
 
-    collection = db['OtelBackend']['Traces']
+    collection = db["OtelBackend"]["Traces"]
     collection.insert_many(trace_dicts)
 
     return "OK", 200
-
-
-def get_trace_dicts(trace_data: Dict) -> List[Dict]:
-    dicts = []
-
-    for resource in trace_data['resourceSpans']:
-        for scope in resource['scopeSpans']:
-            spanIdToPath = {}
-            scope['spans'].sort(key=lambda s : int(s['startTimeUnixNano']))
-            for span in scope['spans']:
-                dicts.append(get_trace_dict(span, spanIdToPath))
-
-    return dicts
-
-
-def get_trace_dict(trace: Dict, spanIdToPath: Dict) -> Dict:
-    dict = {}
-    dict['name'] = trace['name']
-    dict['spanId'] = trace['spanId']
-    dict['responseTime'] = (int(trace['endTimeUnixNano']) -
-                         int(trace['startTimeUnixNano'])) / (10 ** 6)
-    dict['timestamp'] = datetime.utcfromtimestamp(
-        int(trace['startTimeUnixNano'][:10]))
-
-    dict['args'] = []
-    for attribute in trace['attributes']:
-        if attribute['key'] == 'file':
-            dict['file'] = attribute['value']['stringValue']
-        elif attribute['key'] == 'qualName':
-            dict['qualName'] = attribute['value']['stringValue']
-        elif attribute['key'] == 'commit_id':
-            dict['commit_id'] = attribute['value']['stringValue']
-        elif attribute['key'] == 'branch':
-            dict['branch'] = attribute['value']['stringValue']
-        elif attribute['key'] == 'message':
-            dict['commit_message'] = attribute['value']['stringValue']
-        else:
-            # any other attribute is an arg
-            dict['args'].append(attribute)
-            
-    # Add path for retieving whole span tree 
-    if 'parentSpanId' in trace:
-        dict['parentSpanId'] = trace['parentSpanId']
-        dict['path'] = spanIdToPath[dict['parentSpanId']] + ',' + dict['qualName'] + ':' + dict['file']
-    else:
-        dict['path'] = dict['qualName'] + ':' + dict['file']
-        
-    spanIdToPath[dict['spanId']] = dict['path']
-    
-    return dict
 
 
 if __name__ == "__main__":
---
+++ trace_processor.py
@@ -0,0 +1,170 @@
+from dataclasses import dataclass
+from typing import Dict, List, Set
+from google.protobuf import text_format
+from datetime import datetime, timedelta
+import json
+
+
+@dataclass
+class ExecutionPathInfo:
+    root: str
+    span_ids: Set[str]
+    node_to_children: Dict[str, List[str]]
+
+
+def get_trace_dicts(trace_data: Dict) -> List[Dict]:
+    json.dumps(trace_data)
+
+    dicts = []
+
+    for resource in trace_data["resourceSpans"]:
+        for scope in resource["scopeSpans"]:
+            # For
+            spanIdToPath: Dict[str, str] = {}
+            # Sort by start time so we always process classers before callees
+            scope["spans"].sort(key=lambda s: int(s["startTimeUnixNano"]))
+            current_execution_path = {"#all_span_ids": set(), "#root": ""}
+            all_execution_paths = []
+            for span in scope["spans"]:
+                dict = get_standard_trace_dict(span)
+
+                func_id = dict["qualName"] + ":" + dict["file"]
+
+                # Add path for retieving whole span tree
+                if "parentSpanId" in span:
+                    dict["parentSpanId"] = span["parentSpanId"]
+                    print(json.dumps(spanIdToPath))
+                    dict["path"] = spanIdToPath[dict["parentSpanId"]] + "," + func_id
+                    parentName = spanIdToPath[dict["parentSpanId"]].split(",")[-1]
+                    if parentName in current_execution_path:
+                        if func_id not in current_execution_path[parentName]:
+                            current_execution_path[parentName].append(func_id)
+                    else:
+                        current_execution_path[parentName] = [func_id]
+                else:
+                    if len(current_execution_path["#all_span_ids"]) > 1:
+                        print("merging")
+                        all_execution_paths = merge_execution_paths(
+                            all_execution_paths, current_execution_path
+                        )
+                        current_execution_path = {
+                            "#all_span_ids": set([dict["spanId"]]),
+                            "#root": func_id,
+                        }
+                    else:
+                        # current_execution_path['#all_span_ids'].add(dict['spanId'])
+                        current_execution_path["#root"] = func_id
+
+                    dict["path"] = func_id
+
+                print(dict["path"])
+                current_execution_path["#all_span_ids"].add(dict["spanId"])
+                spanIdToPath[dict["spanId"]] = dict["path"]
+
+                dicts.append(dict)
+                print("paths")
+                print(all_execution_paths)
+                print(current_execution_path)
+
+            print(all_execution_paths)
+
+            if len(current_execution_path["#all_span_ids"]) > 1:
+                all_execution_paths = merge_execution_paths(
+                    all_execution_paths, current_execution_path
+                )
+
+            path_strings = []
+            for execution_path in all_execution_paths:
+                path_strings.append(
+                    {
+                        "all_span_ids": execution_path["#all_span_ids"],
+                        "executionPathString": generate_execution_path_string(
+                            execution_path["#root"], execution_path
+                        ),
+                    }
+                )
+
+            print(path_strings)
+
+            for dict in dicts:
+                for path in path_strings:
+                    if dict["spanId"] in path["all_span_ids"]:
+                        dict["executionPathString"] = path["executionPathString"]
+    print(dicts)
+    return dicts
+
+
+# Given list of execution paths and new execution path, add new path if unique
+def merge_execution_paths(all_execution_paths, current_execution_path) -> Dict:
+    if len(all_execution_paths) == 0:
+        all_execution_paths = [current_execution_path]
+    matches = [
+        execution_path_equal(path, current_execution_path)
+        for path in all_execution_paths
+    ]
+    if not all(matches):
+        all_execution_paths.append(current_execution_path)
+    print("merge")
+    print(all_execution_paths)
+
+    return all_execution_paths
+
+
+def execution_path_equal(execution_path1, execution_path2) -> bool:
+    for name, children in execution_path2.items():
+        if name != "#all_span_ids":
+            if name in execution_path1:
+                if len(children) == len(execution_path1[name]):
+                    for i in range(len(children)):
+                        if children[i] != execution_path1[name][i]:
+                            return False
+                else:
+                    return False
+    return len(execution_path1) == len(execution_path2)
+
+
+def generate_execution_path_string(curr: str, nodeChildren: Dict):
+    execution_string = curr
+
+    if curr in nodeChildren:
+        if len(nodeChildren[curr]) > 1:
+            execution_string += "("
+        else:
+            execution_string += ","
+        for i, child in enumerate(nodeChildren[curr]):
+            execution_string += generate_execution_path_string(child, nodeChildren)
+            if i != len(nodeChildren[curr]) - 1:
+                execution_string += "|"
+        if len(nodeChildren[curr]) > 1:
+            execution_string += ")"
+    else:
+        execution_string += ";"
+    return execution_string
+
+
+def get_standard_trace_dict(trace: Dict):
+    dict = {}
+    dict["name"] = trace["name"]
+    dict["spanId"] = trace["spanId"]
+    dict["responseTime"] = (
+        int(trace["endTimeUnixNano"]) - int(trace["startTimeUnixNano"])
+    ) / (10**6)
+    dict["timestamp"] = datetime.utcfromtimestamp(int(trace["startTimeUnixNano"][:10]))
+
+    dict["args"] = []
+    for attribute in trace["attributes"]:
+        if attribute["key"] == "file":
+            dict["file"] = attribute["value"]["stringValue"]
+        elif attribute["key"] == "qualName":
+            dict["qualName"] = attribute["value"]["stringValue"]
+        elif attribute["key"] == "commit_id":
+            dict["commit_id"] = attribute["value"]["stringValue"]
+        elif attribute["key"] == "branch":
+            dict["branch"] = attribute["value"]["stringValue"]
+        elif attribute["key"] == "message":
+            dict["commit_message"] = attribute["value"]["stringValue"]
+        else:
+            # any other attribute is an arg
+            dict["args"].append(attribute)
+
+    return dict
